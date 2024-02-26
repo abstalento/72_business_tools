a2ba945d4797d40d4c206a8b981a520128bca7fd
jaffrin <gopisakthivel@alphabsolutions.com>
2024-02-26 17:37:09 +0530

    implement tailwind css

---
+++ net-profit-margin/pages/index.js
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+// import '../styles/global.css'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h1 className='text-yellow-700 capitalize'>welcome to net profit margin</h1>
+    </div>
+  );
+}
