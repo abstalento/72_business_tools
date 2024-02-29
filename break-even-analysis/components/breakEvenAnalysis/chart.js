6783e53984f721ec90b92c871687683a51a311f3
Raghava <moorthy46030@gmail.com>
2024-02-29 16:12:20 +0530

    component_breakdown

---
+++ break-even-analysis/components/breakEvenAnalysis/chart.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class chart extends Component {
+  render() {
+    return (
+      <div>chart</div>
+    )
+  }
+}
---
+++ break-even-analysis/components/breakEvenAnalysis/input.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class input extends Component {
+  render() {
+    return (
+      <div>input</div>
+    )
+  }
+}
