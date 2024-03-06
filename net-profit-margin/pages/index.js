76504a82cbd4259becc27a57d7165091aa052e96
jaffrin <gopisakthivel@alphabsolutions.com>
2024-03-06 16:39:34 +0530

    test git notification

--- net-profit-margin/pages/index.js
+++ net-profit-margin/pages/index.js
@@ -8,7 +8,7 @@ export default function Home() {
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1 className='text-yellow-700 capitalize'>welcome to net profit margin</h1>
+      <h1 className='text-yellow-700 capitalize'>welcome to net profit margin testing buddy</h1>
     </div>
   );
 }
